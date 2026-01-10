e84750f192358930342ada1b4384abb32dcc45bd
Ishidawg <willianscagol@gmail.com>
2026-01-10 00:53:24 -0300

    fix: replace git clone by urllib

--- scripts_core/clone_core.py
+++ scripts_core/clone_core.py
@@ -1,12 +1,24 @@
 from PySide6.QtCore import QObject, Signal
 import os
 import shutil
-import subprocess
+import urllib.request
+import zipfile
+from pathlib import Path
+# import subprocess
 
-REPO_URLS = {
-  "default": "https://github.com/crosire/reshade-shaders.git",
-  "prod80": "https://github.com/prod80/prod80-ReShade-Repository.git",
-  "quint": "https://github.com/martymcmodding/qUINT.git"
+REPO_INFO = {
+  "default": {
+     "url": "https://github.com/crosire/reshade-shaders.git",
+     "branch": "slim"
+  },
+  "prod80": {
+    "url": "https://github.com/prod80/prod80-ReShade-Repository.git",
+    "branch": "master"
+  },
+  "quint": {
+    "url": "https://github.com/martymcmodding/qUINT.git",
+    "branch": "master"
+  }
 }
 
 TEMP_DIR = "./reshade/temp_clones"
@@ -33,7 +45,7 @@ class CloneWorker(QObject):
 
       shaders_dest = os.path.join(self.game_dir, "Shaders")
       textures_dest = os.path.join(self.game_dir, "Textures")
-      
+
       os.makedirs(shaders_dest, exist_ok=True)
       os.makedirs(textures_dest, exist_ok=True)
 
@@ -44,37 +56,66 @@ class CloneWorker(QObject):
       self.progress_update.emit(0)
 
       for repo_key in self.selected_repos:
-        url = REPO_URLS.get(repo_key)
-        if not url: continue
+        # url = REPO_URLS.get(repo_key)
+        repo_data = REPO_INFO.get(repo_key)
+        if not repo_data: continue
 
         repo_name = repo_key
+        repo_branch = repo_data["branch"]
+        repo_url = repo_data["url"]
+
+        zip_url = f"{repo_url}/archive/refs/heads/{repo_branch}.zip"
+
         self.status_update.emit(f"Cloning {repo_name} repository")
-        
-        temp_repo_path = os.path.join(TEMP_DIR, repo_name)
-        
-        if os.path.exists(temp_repo_path):
-          shutil.rmtree(temp_repo_path)
+
+        zip_path = os.path.join(TEMP_DIR, f"{repo_name}.zip")
+        extract_path = os.path.join(TEMP_DIR, repo_name)
+
+        if os.path.exists(extract_path):
+          shutil.rmtree(extract_path)
 
         try:
-          subprocess.run(["git", "clone", url, temp_repo_path], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        except subprocess.CalledProcessError:
+          # Need to replace git with a python native because MINT 22.2 does not download
+
+          urllib.request.urlretrieve(zip_url, zip_path)
+
+          self.status_update.emit(f"Extracting {repo_name}...")
+          with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(TEMP_DIR)
+
+          extracted_folder_name = self._find_extracted_folder(TEMP_DIR, repo_key)
+
+          if extracted_folder_name:
+            full_extracted_path = os.path.join(TEMP_DIR, extracted_folder_name)
+            shutil.move(full_extracted_path, extrac_path)
+
+          os.remove(zip_path)
+          # subprocess.run(["git", "clone", url, temp_repo_path], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        except Exception as e:
           self.status_update.emit(f"Failed to clone {repo_name}. Skipping.")
           continue
 
         self.status_update.emit(f"Installing {repo_name} files")
-        self._organize_files(temp_repo_path, shaders_dest, textures_dest)
+        self._organize_files(extract_path, shaders_dest, textures_dest)
 
         current_repo += 1
         percentage = int((current_repo / total_repos) * 100)
         self.progress_update.emit(percentage)
 
       shutil.rmtree(TEMP_DIR, ignore_errors=True)
-      
+
       self.status_update.emit("All shaders installed!")
       self.finished.emit()
-
     except Exception as e:
       self.error.emit(str(e))
+
+  def _find_extracted_folder(self, base_path, keyword):
+    for item in os.listdir(base_path):
+      if os.path.isdir(os.path.join(base_path, item)):
+        if item == keyword: continue
+        return item
+
+    return None
 
   def _organize_files(self, source_root, shaders_dest, textures_dest):
     for root, dirs, files in os.walk(source_root):
@@ -83,10 +124,9 @@ class CloneWorker(QObject):
       for file in files:
         file_lower = file.lower()
         src_file = os.path.join(root, file)
-        
+
         if file_lower.endswith(('.fx', '.fxh')):
           shutil.copy2(src_file, os.path.join(shaders_dest, file))
-        
+
         elif file_lower.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tga')):
           shutil.copy2(src_file, os.path.join(textures_dest, file))
-          
