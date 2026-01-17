242dc7acfd1bf86fb2ebb0bd06cdfa3e363177e3
Ishidawg <willianscagol@gmail.com>
2026-01-17 13:34:54 -0300

    feat: add steam and other commands button copy to make it easier

--- widgets/wrapper_widget.py
+++ widgets/wrapper_widget.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import struct
+import pyperclip
 
 from PySide6.QtWidgets import (
   QApplication,
@@ -8,10 +9,11 @@ from PySide6.QtWidgets import (
   QMainWindow,
   QVBoxLayout,
   QHBoxLayout,
-  QWidget
+  QWidget,
+  QPushButton
 )
 
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Slot
 
 class WrapperWidget(QWidget):
 
@@ -21,31 +23,59 @@ class WrapperWidget(QWidget):
     ly = QVBoxLayout()
     ly.setAlignment(Qt.AlignTop | Qt.AlignmentFlag.AlignCenter)
 
+    ly_steam_command = QHBoxLayout()
+    ly_steam_command.setAlignment(Qt.AlignTop | Qt.AlignmentFlag.AlignLeft)
+
+    ly_other_command = QHBoxLayout()
+    ly_other_command.setAlignment(Qt.AlignTop | Qt.AlignmentFlag.AlignLeft)
+
     # Description
-    # l_description = QLabel("As you are installing reshade on a Direct3D 8.0 game, please add the command bellow to your environment variables or steam launch options.")
     l_description = QLabel("As you are installing reshade on a DirectX 8.0 game, you need to set environment variables on steam  or heroic games launcher.")
     l_description.setAlignment(Qt.AlignmentFlag.AlignJustify)
     l_description.setWordWrap(True)
 
     # CSS style to command label
-    s_code = "background-color: #2b2b2b; color: #ffffff; padding: 5px;"
-    s_font = "font 12pt; font-weight: 600; padding: 5px"
+    s_code = "color: #FACE68; padding: 5px; font-style: italic;"
+    s_font = "font 12pt; font-weight: 600; padding: 5px; margin: 5px;"
 
     # Command widget
-    self.l_steam_command = QLabel(f"<html>Steam: <span style='{s_code}'>WINEDLLOVERRIDES='d3d8=n,b' %command%</span></html>")
+    self.l_steam_command = QLabel(f"<html><strong>Steam: <span style='{s_code}'>WINEDLLOVERRIDES='d3d8=n,b' %command%</span></strong></html>")
     self.l_steam_command.setStyleSheet(s_font)
     self.l_steam_command.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
-    self.l_other_command = QLabel(f"<html>Other: <span style='{s_code}'>WINEDLLOVERRIDES='d3d8=n,b</span></html>")
+    self.l_other_command = QLabel(f"<html><strong>Other: <span style='{s_code}'>WINEDLLOVERRIDES='d3d8=n,b</span></strong></html>")
     self.l_other_command.setStyleSheet(s_font)
     self.l_other_command.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
+    # Buttons
+    self.b_steam = QPushButton("Copy steam")
+    self.b_other = QPushButton("Copy other")
+
+    # Connect Functions
+    self.b_steam.clicked.connect(self.copy_steam)
+    self.b_other.clicked.connect(self.copy_other)
 
     # Add widgets
     ly.addWidget(l_description)
     ly.addSpacing(5)
-    ly.addWidget(self.l_steam_command)
-    ly.addSpacing(5)
-    ly.addWidget(self.l_other_command)
+
+    ly_steam_command.addWidget(self.l_steam_command)
+    ly_steam_command.addWidget(self.b_steam)
+
+    ly_other_command.addWidget(self.l_other_command)
+    ly_other_command.addWidget(self.b_other)
+
+    ly.addLayout(ly_steam_command)
+    ly.addLayout(ly_other_command)
 
     self.setLayout(ly)
+  
+  # Functions to copy commands to clipboard
+  @Slot()
+  def copy_steam(self):
+    pyperclip.copy("WINEDLLOVERRIDES='d3d8=n,b' %command%")
+
+  @Slot()
+  def copy_other(self):
+    pyperclip.copy("WINEDLLOVERRIDES='d3d8=n,b'")
+
